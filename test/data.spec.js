978c07f3898cffc8f0d48cf290d365d02bf84d99
Andrea <a.aparicio.g@hotmail.com>
2023-07-17 13:49:32 -0600

    se completaron los test

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,21 +1,193 @@
 import {filterData,sortData,change, mean} from '../src/data.js';
 
+const data =[ 
+  {
+    "num": "151",
+    "name": "mew",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto"
+    },
+    "pokemon-rarity": "mythic",
+    "type": [
+      "psychic"
+    ],
+    "quick-move": [
+      {
+        "name": "pound",
+        "type": "normal",
+        "base-damage": "7",
+        "energy": "6",
+        "move-duration-seg": "0.6"
+      }],
+    "eps" : 10.0,
+    "dps" : 11.7,
+
+  },
+  {
+    "num": "004",
+    "name": "charmander",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "fire"
+    ],
+    "quick-move": [
+      {
+        "name": "ember",
+        "type": "fire",
+        "base-damage": "10",
+        "energy": "10",
+        "move-duration-seg": "1"
+      },
+      {
+        "name": "scratch",
+        "type": "normal",
+        "base-damage": "6",
+        "energy": "4",
+        "move-duration-seg": "0.5"
+      }
+    ],
+    "eps" : 9.0,
+    "dps" : 11.0,
+  },
+  {
+    "num": "007",
+    "name": "squirtle",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "water"
+    ],
+    "quick-move": [
+      {
+        "name": "bubble",
+        "type": "water",
+        "base-damage": "12",
+        "energy": "14",
+        "move-duration-seg": "1.2"
+      },
+      {
+        "name": "tackle",
+        "type": "normal",
+        "base-damage": "5",
+        "energy": "5",
+        "move-duration-seg": "0.5"
+      }
+    ],
+    "eps" : 10.8,
+    "dps" : 10.0,
+  }
+
+];
+
 describe('filterData',() => {
   it('is a function', () => {
     expect(typeof filterData).toBe('function');
   });
   it('filtra correctamente si la propiedad en un objeto', () => {
-    const received = filterData([{type:['fire','dark'],rarity: 'normal',},{type:['water','poison'],rarity: 'mythic',},
-      {type: ['grass','firy','bug'],rarity : 'legendary',}],'water','type');
-    const expected = Array(Object ({rarity: 'mythic', type: ['water', 'poison']}));
+    const received = filterData(data,'water','type');
+    const expected = Array(Object ({
+      "num": "007",
+      "name": "squirtle",
+      "generation": {
+        "num": "generation i",
+        "name": "kanto"
+      },
+      "pokemon-rarity": "normal",
+      "type": [
+        "water"
+      ],
+      "quick-move": [
+        {
+          "name": "bubble",
+          "type": "water",
+          "base-damage": "12",
+          "energy": "14",
+          "move-duration-seg": "1.2"
+        },
+        {
+          "name": "tackle",
+          "type": "normal",
+          "base-damage": "5",
+          "energy": "5",
+          "move-duration-seg": "0.5"
+        }
+      ],
+      "eps" : 10.8,
+      "dps" : 10.0,
+    }));
     expect(received).toEqual(expected);
   });
   it('filtra correctamente si la propiedad es un string',() => {
-    const received = filterData([{name : 'Ares', color: 'black', age : '3 years',},
-      {name : 'Kira', color :'brown', age :'3 years',},
-      {name : 'Minnie', color :'brown', age :'5 years',}],'3 years','age');
-    const expected = Array(Object({name : 'Ares', color: 'black', age : '3 years'}),
-      Object({name : 'Kira', color :'brown', age :'3 years'}));
+    const received = filterData(data,'normal','pokemon-rarity');
+    const expected = Array(Object({ 
+      "num": "004",
+      "name": "charmander",
+      "generation": {
+        "num": "generation i",
+        "name": "kanto"
+      },
+      "pokemon-rarity": "normal",
+      "type": [
+        "fire"
+      ],
+      "quick-move": [
+        {
+          "name": "ember",
+          "type": "fire",
+          "base-damage": "10",
+          "energy": "10",
+          "move-duration-seg": "1"
+        },
+        {
+          "name": "scratch",
+          "type": "normal",
+          "base-damage": "6",
+          "energy": "4",
+          "move-duration-seg": "0.5"
+        }
+      ],
+      "eps" : 9.0,
+      "dps" : 11.0,
+    }),
+    Object({
+      "num": "007",
+      "name": "squirtle",
+      "generation": {
+        "num": "generation i",
+        "name": "kanto"
+      },
+      "pokemon-rarity": "normal",
+      "type": [
+        "water"
+      ],
+      "quick-move": [
+        {
+          "name": "bubble",
+          "type": "water",
+          "base-damage": "12",
+          "energy": "14",
+          "move-duration-seg": "1.2"
+        },
+        {
+          "name": "tackle",
+          "type": "normal",
+          "base-damage": "5",
+          "energy": "5",
+          "move-duration-seg": "0.5"
+        }
+      ],
+      "eps" : 10.8,
+      "dps" : 10.0,
+
+    }));
     expect(received).toEqual(expected);
   });
 });
@@ -25,37 +197,371 @@ describe('sortData',() => {
     expect(typeof sortData).toBe('function');
   });
   it('ordena correctamente de forma ascendente strings al seleccionar una propiedad', () => {
-    const received = sortData([{type:['fire','dark'],rarity: 'normal',},{type:['water','poison'],rarity: 'mythic',},
-      {type: ['grass','firy','bug'],rarity : 'legendary',}],'rarity','ascendente');
-    const expected = Array(Object ({type: ['grass','firy','bug'],rarity : 'legendary'}),
-      Object({type:['water','poison'],rarity: 'mythic'}),Object({type:['fire','dark'],rarity: 'normal'}),);
+    const received = sortData(data,'name','ascendente');
+    const expected = Array(Object (
+      {
+        "num": "004",
+        "name": "charmander",
+        "generation": {
+          "num": "generation i",
+          "name": "kanto"
+        },
+        "pokemon-rarity": "normal",
+        "type": [
+          "fire"
+        ],
+        "quick-move": [
+          {
+            "name": "ember",
+            "type": "fire",
+            "base-damage": "10",
+            "energy": "10",
+            "move-duration-seg": "1"
+          },
+          {
+            "name": "scratch",
+            "type": "normal",
+            "base-damage": "6",
+            "energy": "4",
+            "move-duration-seg": "0.5"
+          }
+        ],
+        "eps" : 9.0,
+        "dps" : 11.0,
+      }
+    ),Object(
+      {
+        "num": "151",
+        "name": "mew",
+        "generation": {
+          "num": "generation i",
+          "name": "kanto"
+        },
+        "pokemon-rarity": "mythic",
+        "type": [
+          "psychic"
+        ],
+        "quick-move": [
+          {
+            "name": "pound",
+            "type": "normal",
+            "base-damage": "7",
+            "energy": "6",
+            "move-duration-seg": "0.6"
+          }],
+        "eps" : 10.0,
+        "dps" : 11.7,
+    
+      }
+    ),Object(
+      {
+        "num": "007",
+        "name": "squirtle",
+        "generation": {
+          "num": "generation i",
+          "name": "kanto"
+        },
+        "pokemon-rarity": "normal",
+        "type": [
+          "water"
+        ],
+        "quick-move": [
+          {
+            "name": "bubble",
+            "type": "water",
+            "base-damage": "12",
+            "energy": "14",
+            "move-duration-seg": "1.2"
+          },
+          {
+            "name": "tackle",
+            "type": "normal",
+            "base-damage": "5",
+            "energy": "5",
+            "move-duration-seg": "0.5"
+          }
+        ],
+        "eps" : 10.8,
+        "dps" : 10.0,
+      }
+    ));
     expect(received).toEqual(expected);
   });
   it('ordena correctamente de forma descendente strings al seleccionar una propiedad',() => {
-    const received = sortData([{name : 'Ares', color: 'black', age : 3,},
-      {name : 'Kira', color :'brown', age :2,},
-      {name : 'Minnie', color :'brown', age :1,}],'name','descendente');
-    const expected = Array(Object({name : 'Minnie', color :'brown', age :1,}),
-      Object({name : 'Kira', color :'brown', age :2}),
-      Object({name : 'Ares', color: 'black', age : 3}));
+    const received = sortData(data,'name','descendente');
+    const expected = Array(Object(
+      {
+        "num": "007",
+        "name": "squirtle",
+        "generation": {
+          "num": "generation i",
+          "name": "kanto"
+        },
+        "pokemon-rarity": "normal",
+        "type": [
+          "water"
+        ],
+        "quick-move": [
+          {
+            "name": "bubble",
+            "type": "water",
+            "base-damage": "12",
+            "energy": "14",
+            "move-duration-seg": "1.2"
+          },
+          {
+            "name": "tackle",
+            "type": "normal",
+            "base-damage": "5",
+            "energy": "5",
+            "move-duration-seg": "0.5"
+          }
+        ],
+        "eps" : 10.8,
+        "dps" : 10.0,
+      }
+    ),
+    Object(
+      {
+        "num": "151",
+        "name": "mew",
+        "generation": {
+          "num": "generation i",
+          "name": "kanto"
+        },
+        "pokemon-rarity": "mythic",
+        "type": [
+          "psychic"
+        ],
+        "quick-move": [
+          {
+            "name": "pound",
+            "type": "normal",
+            "base-damage": "7",
+            "energy": "6",
+            "move-duration-seg": "0.6"
+          }],
+        "eps" : 10.0,
+        "dps" : 11.7,
+    
+      }
+    ),
+    Object (
+      {
+        "num": "004",
+        "name": "charmander",
+        "generation": {
+          "num": "generation i",
+          "name": "kanto"
+        },
+        "pokemon-rarity": "normal",
+        "type": [
+          "fire"
+        ],
+        "quick-move": [
+          {
+            "name": "ember",
+            "type": "fire",
+            "base-damage": "10",
+            "energy": "10",
+            "move-duration-seg": "1"
+          },
+          {
+            "name": "scratch",
+            "type": "normal",
+            "base-damage": "6",
+            "energy": "4",
+            "move-duration-seg": "0.5"
+          }
+        ],
+        "eps" : 9.0,
+        "dps" : 11.0,
+      }
+    ));
     expect(received).toEqual(expected);
   });
   it('ordena correctamente de forma ascendente numeros al seleccionar una propiedad',() => {
-    const received = sortData([{name : 'Ares', color: 'black', age : 3,},
-      {name : 'Kira', color :'brown', age :2,},
-      {name : 'Minnie', color :'brown', age :1,}],'age','ascendente');
-    const expected = Array(Object({name : 'Minnie', color :'brown', age :1,}),
-      Object({name : 'Kira', color :'brown', age :2}),
-      Object({name : 'Ares', color: 'black', age : 3}));
+    const received = sortData(data,'eps','ascendente');
+    const expected = Array(Object (
+      {
+        "num": "004",
+        "name": "charmander",
+        "generation": {
+          "num": "generation i",
+          "name": "kanto"
+        },
+        "pokemon-rarity": "normal",
+        "type": [
+          "fire"
+        ],
+        "quick-move": [
+          {
+            "name": "ember",
+            "type": "fire",
+            "base-damage": "10",
+            "energy": "10",
+            "move-duration-seg": "1"
+          },
+          {
+            "name": "scratch",
+            "type": "normal",
+            "base-damage": "6",
+            "energy": "4",
+            "move-duration-seg": "0.5"
+          }
+        ],
+        "eps" : 9.0,
+        "dps" : 11.0,
+      }
+    ),Object(
+      {
+        "num": "151",
+        "name": "mew",
+        "generation": {
+          "num": "generation i",
+          "name": "kanto"
+        },
+        "pokemon-rarity": "mythic",
+        "type": [
+          "psychic"
+        ],
+        "quick-move": [
+          {
+            "name": "pound",
+            "type": "normal",
+            "base-damage": "7",
+            "energy": "6",
+            "move-duration-seg": "0.6"
+          }],
+        "eps" : 10.0,
+        "dps" : 11.7,
+    
+      }
+    ),Object(
+      {
+        "num": "007",
+        "name": "squirtle",
+        "generation": {
+          "num": "generation i",
+          "name": "kanto"
+        },
+        "pokemon-rarity": "normal",
+        "type": [
+          "water"
+        ],
+        "quick-move": [
+          {
+            "name": "bubble",
+            "type": "water",
+            "base-damage": "12",
+            "energy": "14",
+            "move-duration-seg": "1.2"
+          },
+          {
+            "name": "tackle",
+            "type": "normal",
+            "base-damage": "5",
+            "energy": "5",
+            "move-duration-seg": "0.5"
+          }
+        ],
+        "eps" : 10.8,
+        "dps" : 10.0,
+      }
+    ));
     expect(received).toEqual(expected);
   });
   it('ordena correctamente de forma descendente numeros al seleccionar una propiedad',() => {
-    const received = sortData([{name : 'Ares', color: 'black', age : 3,},
-      {name : 'Kira', color :'brown', age :2,},
-      {name : 'Minnie', color :'brown', age :1,}],'age','descendente');
-    const expected = Array(Object({name : 'Ares', color: 'black', age : 3}),
-      Object({name : 'Kira', color :'brown', age :2}),
-      Object({name : 'Minnie', color :'brown', age :1,}));
+    const received = sortData(data,'eps','descendente');
+    const expected = Array(Object(
+      {
+        "num": "007",
+        "name": "squirtle",
+        "generation": {
+          "num": "generation i",
+          "name": "kanto"
+        },
+        "pokemon-rarity": "normal",
+        "type": [
+          "water"
+        ],
+        "quick-move": [
+          {
+            "name": "bubble",
+            "type": "water",
+            "base-damage": "12",
+            "energy": "14",
+            "move-duration-seg": "1.2"
+          },
+          {
+            "name": "tackle",
+            "type": "normal",
+            "base-damage": "5",
+            "energy": "5",
+            "move-duration-seg": "0.5"
+          }
+        ],
+        "eps" : 10.8,
+        "dps" : 10.0,
+      }
+    ),
+    Object(
+      {
+        "num": "151",
+        "name": "mew",
+        "generation": {
+          "num": "generation i",
+          "name": "kanto"
+        },
+        "pokemon-rarity": "mythic",
+        "type": [
+          "psychic"
+        ],
+        "quick-move": [
+          {
+            "name": "pound",
+            "type": "normal",
+            "base-damage": "7",
+            "energy": "6",
+            "move-duration-seg": "0.6"
+          }],
+        "eps" : 10.0,
+        "dps" : 11.7,
+    
+      }
+    ),
+    Object (
+      {
+        "num": "004",
+        "name": "charmander",
+        "generation": {
+          "num": "generation i",
+          "name": "kanto"
+        },
+        "pokemon-rarity": "normal",
+        "type": [
+          "fire"
+        ],
+        "quick-move": [
+          {
+            "name": "ember",
+            "type": "fire",
+            "base-damage": "10",
+            "energy": "10",
+            "move-duration-seg": "1"
+          },
+          {
+            "name": "scratch",
+            "type": "normal",
+            "base-damage": "6",
+            "energy": "4",
+            "move-duration-seg": "0.5"
+          }
+        ],
+        "eps" : 9.0,
+        "dps" : 11.0,
+      }
+    ));
     expect(received).toEqual(expected);
   });
 });
@@ -65,8 +571,8 @@ describe('change',()=>{
     expect(typeof change).toBe('function');
   });
   it('calcula el eps correctamente',() => {
-    const received = change({'energy': '5','time':'2','damage':10},"energy","time");
-    const expected = parseFloat(5/2).toFixed(1);
+    const received = change(data[2]["quick-move"][0],"energy","move-duration-seg");
+    const expected = "11.7";
     expect(received).toEqual(expected);
 
   });
